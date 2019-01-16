0f7ccf2d13af6681d943cdd514607787b2bc1d26
svanderest <svanderest@ncstudents.niagaracollege.ca>
2019-01-16 15:04:37 -0500

    basic UI and calculator

---
+++ BMICalculator/BMICalculator/BmiCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    class BmiCalculator
+    {
+        public int Height;
+        public int Weight;
+        public bool IsImperial;
+
+        public BmiCalculator(int height, int weight, bool isImperial = false)
+        {
+            Height = height;
+            Weight = weight;
+            IsImperial = isImperial;
+        }
+
+        public string Result
+        {
+            get
+            {
+                double index = Weight / Math.Pow(Height, 2) * (IsImperial ? 703 : 1);
+                if (index < 15)
+                    return "Very severly underweight";
+                else if (index < 16)
+                    return "Severly underweight";
+                else if (index < 18.5)
+                    return "Underweight";
+                else if (index < 25)
+                    return "Normal";
+                else if (index < 30)
+                    return "Overweight";
+                else if (index < 35)
+                    return "Moderately obese";
+                else if (index < 40)
+                    return "Severly obese";
+                else
+                    return "Very severly obese";
+            }
+        }
+    }
+}
--- BMICalculator/BMICalculator/MainPage.xaml.cs
+++ BMICalculator/BMICalculator/MainPage.xaml.cs
@@ -26,5 +26,15 @@ namespace BMICalculator
         {
             this.InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int Height;
+            int Weight;
+            if (int.TryParse(txtHeight.Text, out Height) && int.TryParse(txtWeight.Text, out Weight))
+                lblResult.Text = (new BmiCalculator(Height, Weight)).Result;
+            else
+                lblResult.Text = "Please enter whole number values for height and weight";
+        }
     }
 }
